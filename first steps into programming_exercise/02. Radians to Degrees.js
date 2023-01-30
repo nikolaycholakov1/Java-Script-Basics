c6db5a40fa00bccb2ac97d43009e8a00dcdd2a9c
Nikolay Cholakov <106109220+nikolaycholakov1@users.noreply.github.com>
2023-01-30 23:28:00 +0200

    Delete first steps into programming_exercise directory

--- first steps into programming_exercise/02. Radians to Degrees.js
+++
@@ -1,8 +0,0 @@
-function radiansToDegrees(input) {
-  let radians = input[0];
-  let degrees = (radians * 180) / Math.PI;
-  console.log(degrees);
-}
-
-radiansToDegrees(["3.1416"]);
-radiansToDegrees(["6.2832"]);
